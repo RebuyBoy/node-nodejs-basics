7aa3b9433ad25696f140d2d19b7df6d75014ae88
Sergey M <marchuk.se@gmail.com>
2022-06-01 20:58:52 +0700

    feat: cli task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,12 @@
 export const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  let result = "";
+  for (let i = 0; i < args.length; i += 2) {
+    const propName = args[i].slice(2);
+    const value = args[i + 1];
+    const delimiter = i < args.length - 2 ? ", " : "";
+    result += `${propName} is ${value}${delimiter}`
+  }
+  console.log(result);
 };
+parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,15 @@
+// implement function that parses environment variables with prefix RSS_ and prints them to the console in the format RSS_name1=value1; RSS_name2=value2
 export const parseEnv = () => {
-    // Write your code here 
+
+  console.log(Object.keys(process.env))
+  const environments = process.env;
+  const result = Object.keys(environments)
+    .filter(key => key.indexOf('RSS_') !== -1)
+    .reduce((res, key, i, array) => {
+      return res += (`${key}=${environments[key]}${i < array.length - 1 ? "; " : ""}`)
+    }, "");
+
+  console.log(result);
 };
+
+parseEnv();
