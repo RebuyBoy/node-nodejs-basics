4b110ecc1951387884872628719e0718b79e302a
Sergey M <marchuk.se@gmail.com>
2022-06-03 22:30:45 +0700

    feat: child_process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,12 @@
+import {fork} from "child_process";
+import {fileURLToPath} from "url";
+import {join, dirname} from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const scriptPath = join(__dirname, "files", "script.js");
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  fork(scriptPath, args);
 };
+spawnChildProcess(["aa", "bb", "cc", "dd"]);
