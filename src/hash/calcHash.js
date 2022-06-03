6b73eb9306b540c8431a4b4893119661cbd6bc8e
Sergey M <marchuk.se@gmail.com>
2022-06-03 17:55:34 +0700

    refactor: hash/calculateHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,23 +1,21 @@
 import {readFile} from 'fs/promises'
 import {fileURLToPath} from 'url'
 import {join, dirname} from 'path'
+
 const {createHash} = await import('crypto');
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
 
 export const calculateHash = async () => {
   try {
-    const __dirname = dirname(fileURLToPath(import.meta.url));
-    const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
-
     const file = await readFile(filePath, 'utf-8');
-    const hash = createHash('sha256');
-    hash.update(file);
-    const hashHex = hash.digest('hex');
-    console.log(hashHex);
-    return hashHex;
+    return createHash('sha256')
+      .update((file))
+      .digest('hex');
   } catch (err) {
-    throw new Error('Something wrong');
+    console.error(`${err.name}: ${err.message}`);
   }
 };
 
-calculateHash();
+console.log(await calculateHash());
