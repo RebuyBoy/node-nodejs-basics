1f2d4e47babfd89709a2230f6bdf15f8b5d54d87
Sergey M <marchuk.se@gmail.com>
2022-06-04 22:38:20 +0700

    refactor: cli another one

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,12 +1,8 @@
-// implement function that parses environment variables with prefix RSS_ and prints them to the console in the format RSS_name1=value1; RSS_name2=value2
 export const parseEnv = () => {
-
-  const environments = process.env;
-  const result = Object.entries(environments)
+  return Object.entries(process.env)
     .filter(([key]) => key.startsWith("RSS_"))
     .map(([key, value]) => `${key}=${value}`)
     .join("; ")
-  console.log(result);
 };
 
-parseEnv();
+console.log(parseEnv());
